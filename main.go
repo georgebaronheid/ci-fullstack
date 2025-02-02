0714330566e704f7b153a7cc1327b3d8a772e16d
George Baronheid <baronheid.george@gmail.com>
2025-02-02 11:16:02 -0300

    fix(): remove operations.go file

--- main.go
+++ main.go
@@ -11,3 +11,22 @@ func main() {
 	fmt.Println(Multiplicacao(10, 10))
 	fmt.Println(Divisao(10, 10))
 }
+
+func Soma(a, b int) int {
+	return a + b
+}
+
+func Subtracao(a, b int) int {
+	return a - b
+}
+
+func Multiplicacao(a, b int) int {
+	return a * b
+}
+
+func Divisao(a, b int) int {
+	if b == 0 {
+		return 0
+	}
+	return a / b
+}
